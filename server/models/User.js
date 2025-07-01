eda12968640ab45c11668c8e721c71047340e091
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-07-01 23:43:40 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,119 +1,3 @@
-// // server/models/User.js
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcryptjs';
-
-// const UserSchema = new mongoose.Schema({
-//   username: {
-//     type: String,
-//     required: [true, 'Username is required'],
-//     unique: true,
-//     trim: true,
-//     minlength: [3, 'Username must be at least 3 characters']
-//   },
-//   email: {
-//     type: String,
-//     required: [true, 'Email is required'],
-//     unique: true,
-//     match: [
-//       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-//       'Please provide a valid email address'
-//     ]
-//   },
-//   password: {
-//     type: String,
-//     required: [true, 'Password is required'],
-//     minlength: [6, 'Password must be at least 6 characters'],
-//     select: false // Don't return password in queries by default
-//   },
-//   firstName: {
-//     type: String,
-//     trim: true
-//   },
-//   lastName: {
-//     type: String,
-//     trim: true
-//   },
-//   role: {
-//     type: String,
-//     enum: ['admin', 'content', 'instructor', 'crm', 'manager'],
-//     default: 'crm'
-//   },
-//   isActive: {
-//     type: Boolean,
-//     default: true
-//   },
-//   assignedTechStacks: [{
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'TechStack'
-//   }],
-//   crmAffiliation: { // For CRM users, to link them to a specific CRM
-//     type: String,
-//     trim: true
-//   },
-//   lastLogin: {
-//     type: Date,
-//     default: null
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now
-//   },
-//   emailPreferences: { // Added for email opt-in/opt-out
-//     reminders: {
-//       dailyTasks: {
-//         enabled: { type: Boolean, default: true }
-//       }
-//     }
-//   },
-//   updatedAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// // Pre-save hook to hash password before saving
-// UserSchema.pre('save', async function(next) {
-//   // Only hash the password if it's been modified (or is new)
-//   if (!this.isModified('password')) return next();
-  
-//   try {
-//     // Generate salt
-//     const salt = await bcrypt.genSalt(10);
-//     // Hash password with salt
-//     this.password = await bcrypt.hash(this.password, salt);
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// // Update the updatedAt timestamp
-// UserSchema.pre('save', function(next) {
-//   this.updatedAt = Date.now();
-//   next();
-// });
-
-// // Method to compare password
-// UserSchema.methods.comparePassword = async function(candidatePassword) {
-//   try {
-//     return await bcrypt.compare(candidatePassword, this.password);
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// };
-
-// // Generate display name
-// UserSchema.virtual('displayName').get(function() {
-//   if (this.firstName && this.lastName) {
-//     return `${this.firstName} ${this.lastName}`;
-//   }
-//   return this.username;
-// });
-
-// const User = mongoose.model('User', UserSchema);
-
-// export default User;
-
 
 // server/models/User.js
 import mongoose from 'mongoose';
@@ -168,8 +52,12 @@ const UserSchema = new mongoose.Schema({
     enum: ['none', 'view', 'edit'],
     default: 'none'
   },
-  // --- START NEW FIELD ---
   canAccessCriticalPoints: {
+    type: Boolean,
+    default: false
+  },
+  // --- START NEW FIELD ---
+  canAccessPostInternships: {
     type: Boolean,
     default: false
   },
