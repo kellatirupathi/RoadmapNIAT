c8bd9f5462057be7030f273aaf6c174a61f5ab37
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-07-09 23:33:09 +0530

    Update InternshipMaster.js

--- server/models/InternshipMaster.js
+++ server/models/InternshipMaster.js
@@ -1,30 +1,3 @@
-// // server/models/InternshipMaster.js
-// import mongoose from 'mongoose';
-
-// const TechProgressSchema = new mongoose.Schema({
-//   techStackName: { type: String, required: true },
-//   // ADDED: Field for manual progress override
-//   manualProgress: { type: Number, default: null, min: 0, max: 100 }
-// }, { _id: false });
-
-// const InternshipMasterSchema = new mongoose.Schema({
-//   companies: { type: String, default: '' },
-//   roles: { type: String, default: '' },
-//   internshipOffers: { type: Number, default: 0 },
-//   companyStatus: { type: String, enum: ['Active', 'Inactive', ''], default: 'Active' },
-//   reasonInactive: { type: String, default: '' },
-//   studentMappingMethod: { type: String, default: '' },
-//   studentMappingCounts: { type: Number, default: 0 },
-//   internshipStartDate: { type: Date, default: null },
-//   stackCompletionDate: { type: Date, default: null },
-//   techProgress: [TechProgressSchema]
-// }, { timestamps: true });
-
-// const InternshipMaster = mongoose.model('InternshipMaster', InternshipMasterSchema);
-// export default InternshipMaster;
-
-
-
 import mongoose from 'mongoose';
 
 // Schema for each tech stack mapping within a company
