ace2af9e59fc04b7896365cc339eea075f6b13d4
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2025-07-09 21:05:55 +0530

    Update criticalPointsService.js

--- client/src/services/criticalPointsService.js
+++ client/src/services/criticalPointsService.js
@@ -3,7 +3,7 @@ import api from './api';
 
 const basePaths = {
     interactions: '/critical-points/interactions',
-    companyStatus: '/critical-points/company-status',
+    // --- MODIFICATION: Removed companyStatus path ---
 };
 
 // --- Interactions Feedback Service ---
@@ -18,16 +18,6 @@ const interactionsService = {
   deleteSubInteraction: (id, subId) => api.delete(`${basePaths.interactions}/${id}/sub/${subId}`),
 };
 
-// --- Company Status Service ---
-const companyStatusService = {
-  getAll: () => api.get(basePaths.companyStatus),
-  create: (data) => api.post(basePaths.companyStatus, data),
-  update: (id, data) => api.put(`${basePaths.companyStatus}/${id}`, data),
-  delete: (id) => api.delete(`${basePaths.companyStatus}/${id}`),
-  bulkUpload: (data) => api.post(`${basePaths.companyStatus}/bulk`, data),
-  // --- NEW SERVICE FUNCTION ---
-  updateStudentStatus: (companyId, studentId, newStatus) => 
-    api.put(`${basePaths.companyStatus}/${companyId}/students/${studentId}/status`, { newStatus })
-};
+// --- MODIFICATION: Removed entire companyStatusService export ---
 
-export { interactionsService, companyStatusService };
+export { interactionsService };
